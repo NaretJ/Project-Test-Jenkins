13e94c56d7b9be92400fefdc5e1032b9c1c9ef72
Naret Jaroenphum <naret@orcsoft.co.th>
2020-09-28 15:04:11 +0700

    Initail project for test jenkins build

---
+++ project-test-jenkins/src/main/java/com/test/jenkins/ProjectTestJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.test.jenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectTestJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectTestJenkinsApplication.class, args);
+	}
+
+}
